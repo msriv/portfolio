4fee1e7d0ca46a63576873fc15b4bddba5ad0726
Mihir Srivastava <msriv@digitaltakeoff.in>
2020-09-02 00:39:36 +0530

    Bug fixes

--- client/pages/api/hello.ts
+++ client/pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+// TODO: To be deleted.
 export default (req: any, res: any) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
